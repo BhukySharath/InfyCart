ab27068f9992c4ab2d07d20dd2a1db85162a8e53
BhukyaSharath <bhukyasharath1729@gmail.com>
2024-02-12 16:09:00 +0530

    allowed host enabled

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-86zk0_7y&tbut%92i&k&2!z=+-jjn4nz@kv=f-*19rxloxv-c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
